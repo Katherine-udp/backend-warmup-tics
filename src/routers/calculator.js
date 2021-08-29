e823d4dee11a569ed5e34055a5d1c1a7cbb038a7
Katherine Huidobro <Khaterine UwU>
2021-08-28 21:49:58 -0400

    final

--- src/routers/calculator.js
+++ src/routers/calculator.js
@@ -3,11 +3,7 @@ const calculatorController = require('../controllers/calculator');
 
 const router =  express.Router();
 
-//   /calculator/add
 router.post('/remuneracionmensual',calculatorController.remuneracionmensual);
-
-
 router.post('/apv',calculatorController.apv);
-
 
 module.exports = router;
